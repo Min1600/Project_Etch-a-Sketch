b27272e842dc0bd9ef70283545f46e5aba410aab
Min1600 <aminhassan00@gmail.com>
2024-06-30 12:12:24 +0100

    Container fits rows and columns evenly

--- Index.js
+++ Index.js
@@ -4,10 +4,21 @@ const container = document.querySelector(".container")
 
 function repeat(n){
     let repeatSquare = ""
-    for (let i=0; i<n; i++){
+    let height = 1;
+    let width = 1;
+    for (let i=0; i<n*n; i++){
         repeatSquare += '<div class="square"></div>';
+        height = n*18
+        width = n*18
     }
-    container.innerHTML = repeatSquare
+    container.innerHTML = repeatSquare;
+    let heigthString =  height.toString()
+    let heigthFinal = heigthString.concat("px")
+    let widthString =  width.toString()
+    let widthFinal = widthString.concat("px")
+    container.style.height = heigthFinal
+    container.style.width = widthFinal 
+
 }
 
 repeat(10)
